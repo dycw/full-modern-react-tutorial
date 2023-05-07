4079841351a095d586c98f8b6cd1879ef487aba2
Derek Wan <d.wan@icloud.com>
2023-05-08 07:33:23 +0800

    Switch to React tutorial (#6)
    * Switch to React
    
    * Bump minor

--- sandbox.js
+++
@@ -1,9 +0,0 @@
-const content = document.querySelectorAll("p");
-const classes = ["success", "error"];
-classes.forEach((cls) => {
-  content.forEach((tag) => {
-    if (tag.innerText.includes(cls)) {
-      tag.classList.add(cls);
-    }
-  });
-});
