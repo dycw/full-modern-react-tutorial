fb96652419c9244136ad24e6c7ebd0cfd6803fa0
Derek Wan <d.wan@icloud.com>
2023-05-08 14:05:20 +0800

    Finish lesson 14 (useEffect hook, the basics)

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -20,6 +20,10 @@ const Home = () => {
       }),
     );
   };
+
+  useEffect(() => {
+    console.log("use effect ran");
+  });
 
   return (
     <div className="home">
