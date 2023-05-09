00125bf805d606c3af516161ee2ca8ad603f72c5
Derek Wan <d.wan@icloud.com>
2023-05-09 15:10:53 +0800

    Finish lesson 27 (controlled inputs, forms)

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          type="text"
+          value={title}
+        />
+        <label>Blog body:</label>
+        <textarea
+          onChange={(e) => setBody(e.target.value)}
+          required
+          value={body}
+        ></textarea>
+        <label>Blog author:</label>
+        <select onChange={(e) => setAuthor(e.target.value)} value={author}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
