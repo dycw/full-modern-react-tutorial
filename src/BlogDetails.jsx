7380be2a10f589ad03b5e6947b14fda990ebebb3
Derek Wan <d.wan@icloud.com>
2023-05-11 07:52:02 +0800

    Finish lesson 31 (deleting blogs)

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -8,6 +9,15 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, { method: "DELETE" }).then(
+      () => {
+        history.push("/");
+      },
+    );
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
