3255b24e087067610458598a67242924b8b15a6b
Derek Wan <d.wan@icloud.com>
2023-05-11 08:00:52 +0800

    Finish lesson 31 (deleting blogs) (#35) (#37)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import BlogDetails from "./BlogDetails";
 import Create from "./Create";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import NotFound from "./NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
